fbe621fc3d7915766417647c2f1a27c29c24ba7c
Martin Sucha <martin.sucha@kiwi.com>
2019-11-02 13:22:01 +0100

    Add benchmark for marshaling

--- tests/benchmark_test.go
+++ tests/benchmark_test.go
@@ -11,132 +11,133 @@ import (
 )
 
 var benchmarkUnmarshalOut BenchmarkStruct
+var benchmarkMarshalOut []byte
+
+var benchTypeInfo = gocql.UDTTypeInfo{
+	NativeType: gocql.NewNativeType(3, gocql.TypeUDT, ""),
+	KeySpace:   "ks",
+	Name:       "MyUDT",
+	Elements: []gocql.UDTField{
+		{
+			Name: "Int",
+			Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
+		},
+		{
+			Name: "Int8",
+			Type: gocql.NewNativeType(2, gocql.TypeTinyInt, ""),
+		},
+		{
+			Name: "Int16",
+			Type: gocql.NewNativeType(2, gocql.TypeSmallInt, ""),
+		},
+		{
+			Name: "Int32",
+			Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
+		},
+		{
+			Name: "Int64",
+			Type: gocql.NewNativeType(2, gocql.TypeBigInt, ""),
+		},
+		{
+			Name: "Uint",
+			Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
+		},
+		{
+			Name: "Uint8",
+			Type: gocql.NewNativeType(2, gocql.TypeTinyInt, ""),
+		},
+		{
+			Name: "Uint16",
+			Type: gocql.NewNativeType(2, gocql.TypeSmallInt, ""),
+		},
+		{
+			Name: "Uint32",
+			Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
+		},
+		{
+			Name: "Uint64",
+			Type: gocql.NewNativeType(2, gocql.TypeBigInt, ""),
+		},
+		{
+			Name: "Bool",
+			Type: gocql.NewNativeType(2, gocql.TypeBoolean, ""),
+		},
+		{
+			Name: "String",
+			Type: gocql.NewNativeType(2, gocql.TypeVarchar, ""),
+		},
+		{
+			Name: "Float32",
+			Type: gocql.NewNativeType(2, gocql.TypeFloat, ""),
+		},
+		{
+			Name: "Float64",
+			Type: gocql.NewNativeType(2, gocql.TypeDouble, ""),
+		},
+		{
+			Name: "BigInt",
+			Type: gocql.NewNativeType(2, gocql.TypeVarint, ""),
+		},
+		{
+			Name: "NamedInt",
+			Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
+		},
+		{
+			Name: "NamedInt8",
+			Type: gocql.NewNativeType(2, gocql.TypeTinyInt, ""),
+		},
+		{
+			Name: "NamedInt16",
+			Type: gocql.NewNativeType(2, gocql.TypeSmallInt, ""),
+		},
+		{
+			Name: "NamedInt32",
+			Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
+		},
+		{
+			Name: "NamedInt64",
+			Type: gocql.NewNativeType(2, gocql.TypeBigInt, ""),
+		},
+		{
+			Name: "NamedUint",
+			Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
+		},
+		{
+			Name: "NamedUint8",
+			Type: gocql.NewNativeType(2, gocql.TypeTinyInt, ""),
+		},
+		{
+			Name: "NamedUint16",
+			Type: gocql.NewNativeType(2, gocql.TypeSmallInt, ""),
+		},
+		{
+			Name: "NamedUint32",
+			Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
+		},
+		{
+			Name: "NamedUint64",
+			Type: gocql.NewNativeType(2, gocql.TypeBigInt, ""),
+		},
+		{
+			Name: "NamedBool",
+			Type: gocql.NewNativeType(2, gocql.TypeBoolean, ""),
+		},
+		{
+			Name: "NamedString",
+			Type: gocql.NewNativeType(2, gocql.TypeVarchar, ""),
+		},
+		{
+			Name: "NamedFloat32",
+			Type: gocql.NewNativeType(2, gocql.TypeFloat, ""),
+		},
+		{
+			Name: "NamedFloat64",
+			Type: gocql.NewNativeType(2, gocql.TypeDouble, ""),
+		},
+	},
+}
 
 func BenchmarkUnmarshal(b *testing.B) {
-	info := gocql.UDTTypeInfo{
-		NativeType: gocql.NewNativeType(3, gocql.TypeUDT, ""),
-		KeySpace:   "ks",
-		Name:       "MyUDT",
-		Elements: []gocql.UDTField{
-			{
-				Name: "Int",
-				Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
-			},
-			{
-				Name: "Int8",
-				Type: gocql.NewNativeType(2, gocql.TypeTinyInt, ""),
-			},
-			{
-				Name: "Int16",
-				Type: gocql.NewNativeType(2, gocql.TypeSmallInt, ""),
-			},
-			{
-				Name: "Int32",
-				Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
-			},
-			{
-				Name: "Int64",
-				Type: gocql.NewNativeType(2, gocql.TypeBigInt, ""),
-			},
-			{
-				Name: "Uint",
-				Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
-			},
-			{
-				Name: "Uint8",
-				Type: gocql.NewNativeType(2, gocql.TypeTinyInt, ""),
-			},
-			{
-				Name: "Uint16",
-				Type: gocql.NewNativeType(2, gocql.TypeSmallInt, ""),
-			},
-			{
-				Name: "Uint32",
-				Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
-			},
-			{
-				Name: "Uint64",
-				Type: gocql.NewNativeType(2, gocql.TypeBigInt, ""),
-			},
-			{
-				Name: "Bool",
-				Type: gocql.NewNativeType(2, gocql.TypeBoolean, ""),
-			},
-			{
-				Name: "String",
-				Type: gocql.NewNativeType(2, gocql.TypeVarchar, ""),
-			},
-			{
-				Name: "Float32",
-				Type: gocql.NewNativeType(2, gocql.TypeFloat, ""),
-			},
-			{
-				Name: "Float64",
-				Type: gocql.NewNativeType(2, gocql.TypeDouble, ""),
-			},
-			{
-				Name: "BigInt",
-				Type: gocql.NewNativeType(2, gocql.TypeVarint, ""),
-			},
-			{
-				Name: "NamedInt",
-				Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
-			},
-			{
-				Name: "NamedInt8",
-				Type: gocql.NewNativeType(2, gocql.TypeTinyInt, ""),
-			},
-			{
-				Name: "NamedInt16",
-				Type: gocql.NewNativeType(2, gocql.TypeSmallInt, ""),
-			},
-			{
-				Name: "NamedInt32",
-				Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
-			},
-			{
-				Name: "NamedInt64",
-				Type: gocql.NewNativeType(2, gocql.TypeBigInt, ""),
-			},
-			{
-				Name: "NamedUint",
-				Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
-			},
-			{
-				Name: "NamedUint8",
-				Type: gocql.NewNativeType(2, gocql.TypeTinyInt, ""),
-			},
-			{
-				Name: "NamedUint16",
-				Type: gocql.NewNativeType(2, gocql.TypeSmallInt, ""),
-			},
-			{
-				Name: "NamedUint32",
-				Type: gocql.NewNativeType(2, gocql.TypeInt, ""),
-			},
-			{
-				Name: "NamedUint64",
-				Type: gocql.NewNativeType(2, gocql.TypeBigInt, ""),
-			},
-			{
-				Name: "NamedBool",
-				Type: gocql.NewNativeType(2, gocql.TypeBoolean, ""),
-			},
-			{
-				Name: "NamedString",
-				Type: gocql.NewNativeType(2, gocql.TypeVarchar, ""),
-			},
-			{
-				Name: "NamedFloat32",
-				Type: gocql.NewNativeType(2, gocql.TypeFloat, ""),
-			},
-			{
-				Name: "NamedFloat64",
-				Type: gocql.NewNativeType(2, gocql.TypeDouble, ""),
-			},
-		},
-	}
-
 	fieldValues := [][]byte{
 		[]byte("\x12\x34\x56\x78"),                 // int
 		[]byte("\x12"),                             // int8
@@ -178,9 +179,54 @@ func BenchmarkUnmarshal(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		err := gocql.Unmarshal(info, data, &benchmarkUnmarshalOut)
+		err := gocql.Unmarshal(benchTypeInfo, data, &benchmarkUnmarshalOut)
 		if err != nil {
 			b.Fatal(err)
 		}
+	}
+}
+
+func BenchmarkMarshal(b *testing.B) {
+	s := BenchmarkStruct{
+		Int:          0x12345678,
+		Int8:         0x12,
+		Int16:        0x1234,
+		Int32:        0x12345678,
+		Int64:        0x123456789abcdeef,
+		Uint:         0x12345678,
+		Uint8:        0x12,
+		Uint16:       0x1234,
+		Uint32:       0x12345678,
+		Uint64:       0x123456789abcdeef,
+		Bool:         true,
+		String:       "hello world",
+		Float32:      0,
+		Float64:      0,
+		BigInt:       *big.NewInt(0x123456789abcdeef),
+		Dec:          *inf.NewDec(0x123456789abcdeef, 1),
+		NamedInt:     0x12345678,
+		NamedInt8:    0x12,
+		NamedInt16:   0x1234,
+		NamedInt32:   0x12345678,
+		NamedInt64:   0x123456789abcdeef,
+		NamedUint:    0x12345678,
+		NamedUint8:   0x12,
+		NamedUint16:  0x1234,
+		NamedUint32:  0x12345678,
+		NamedUint64:  0x123456789abcdeef,
+		NamedBool:    true,
+		NamedString:  "hello world",
+		NamedFloat32: 0,
+		NamedDouble:  0,
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		data, err := gocql.Marshal(benchTypeInfo, s)
+		if err != nil {
+			b.Fatal(err)
+		}
+		benchmarkMarshalOut = data
 	}
 }
